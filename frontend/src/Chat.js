313b420f834b4d5f22864a728e2d858f620f5ad6
YEAHJEAN <uv1030@naver.com>
2024-06-09 00:18:52 +0900

    FIX Chat

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -8,6 +8,7 @@ const socket = io('http://43.202.124.253:3001');
 
 function Chat() {
   const { chatroom, username } = useParams();
+  const userId = 123; // 예시로 userId를 정의
   const [currentChat, setCurrentChat] = useState(chatroom || 'general');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
@@ -53,25 +54,26 @@ function Chat() {
   };
 
   return (
-      <div className="chat-container">
-        <h1>Chat</h1>
-        <hr /> {/* 가로 경계선 추가 */}
-        {userJoined && <div className="user-joined">{userJoined}</div>}
-        {messages.map((msg, index) => (
-          <div className={`message ${msg.username === username ? 'sent-message' : 'received-message'}`} key={index}>
-            <div className="message-bubble">
-              <span className="username">{msg.username}:</span> {msg.message}
-            </div>
-            <span className="timestamp">({new Date(msg.timestamp).getHours()}:{new Date(msg.timestamp).getMinutes()})</span>
+    <div className="chat-container">
+      <h1>Chat</h1>
+      <ChatroomList userId={userId} /> {/* ChatroomList 컴포넌트 사용 */}
+      <hr /> {/* 가로 경계선 추가 */}
+      {userJoined && <div className="user-joined">{userJoined}</div>}
+      {messages.map((msg, index) => (
+        <div className={`message ${msg.username === username ? 'sent-message' : 'received-message'}`} key={index}>
+          <div className="message-bubble">
+            <span className="username">{msg.username}:</span> {msg.message}
           </div>
-        ))}
-        <hr /> {/* 가로 경계선 추가 */}
-        <div className="message-input-container">
-          <input type="text" className="message-input" value={message} onChange={(e) => setMessage(e.target.value)} />
-          <button onClick={sendMessage} className="send-button">Send</button>
+          <span className="timestamp">({new Date(msg.timestamp).getHours()}:{new Date(msg.timestamp).getMinutes()})</span>
         </div>
+      ))}
+      <div className="bottom-hr" /> {/* 밑에 고정된 가로 경계선 */}
+      <div className="message-input-container">
+        <input type="text" className="message-input" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={sendMessage} className="send-button">Send</button>
       </div>
-  );  
+    </div>
+  ); 
 }
 
 export default Chat;
