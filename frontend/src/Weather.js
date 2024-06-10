14e2bd801332b4df00b647abae2c9297ce8c0085
YEAHJEAN <uv1030@naver.com>
2024-06-10 17:05:55 +0900

    FIX Weather.js and .css

--- frontend/src/Weather.js
+++ frontend/src/Weather.js
@@ -35,28 +35,31 @@ const Weather = () => {
     navigate('/home');
   };
 
-  // 섭씨로 변환하는 함수
   const kelvinToCelsius = (kelvin) => {
     return (kelvin - 273.15).toFixed(2);
   };
 
-  // 날씨 정보가 있는 경우 표시할 내용
   return (
-    <div>
-      <button onClick={handleClick} style={{ cursor: 'pointer', border: 'none', background: 'none', width: '300px', display: 'block', margin: '0 auto', outline: 'none' }}>
+    <div className="weather-page-wrapper">
+      <button 
+        className="weather-button" 
+        onClick={handleClick} 
+        style={{ cursor: 'pointer', border: 'none', background: 'none', outline: 'none' }}>
         <img src="Home.jpg" alt="Go to Home" style={{ width: '250px', height: '120px' }} />
       </button>
-      <div className="weather-container">
-        <h2 className="weather-title">Weather in {weatherData.name}</h2>
-        <div className="weather-info">
-          <p className="weather-temp">Temperature: {kelvinToCelsius(weatherData.main.temp)} °C</p>
-          <p className="weather-desc">Description: {weatherData.weather[0].description}</p>
+      <div className="weather-page">
+        <div className="weather-container">
+          <h2 className="weather-title">Weather in {weatherData.name}</h2>
+          <div className="weather-info">
+            <p className="weather-temp">Temperature: {kelvinToCelsius(weatherData.main.temp)} °C</p>
+            <p className="weather-desc">Description: {weatherData.weather[0].description}</p>
+          </div>
+          <img
+            className="weather-icon"
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            alt="Weather Icon"
+          />
         </div>
-        <img
-          className="weather-icon"
-          src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-          alt="Weather Icon"
-        />
       </div>
     </div>
   );
